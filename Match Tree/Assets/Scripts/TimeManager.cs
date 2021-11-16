4cc3bbf3b0b8d18ba7d82c8124a2c14efc1a0271
LAPTOP-M8OJ4LN0\Juanraden <juanraden@gmail.com>
2021-11-16 22:07:36 +0700

    1.0

---
+++ Match Tree/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    #region Singleton
+
+    private static TimeManager _instance = null;
+
+    public static TimeManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<TimeManager>();
+
+                if (_instance == null)
+                {
+                    Debug.LogError("Fatal Error: TimeManager not Found");
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    #endregion
+
+    public int duration;
+
+    private float time;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        time = 0;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (GameFlowManager.Instance.IsGameOver)
+        {
+            return;
+        }
+
+        if (time > duration)
+        {
+            Debug.Log("GAME OVER");
+            GameFlowManager.Instance.GameOver();
+            return;
+        }
+
+        time += Time.deltaTime;
+    }
+
+    public float GetRemainingTime()
+    {
+        return duration - time;
+    }
+}
